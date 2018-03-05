1128a76fbebe6786a2dfa4c39b3d72ee6aa23e2c
王龙 <cn_percy_wang@163.com>
2018-03-05 23:17:23 +0800

    修正包路径

--- src/main/resources/template/src/main/java/${basepackage_dir}/service/mapper/dataobject/${className}DO.java
+++ src/main/resources/template/src/main/java/${basepackage_dir}/service/mapper/dataobject/${className}DO.java
@@ -1,16 +1,16 @@
 <#include "/java_copyright.include">
 <#assign className = table.className>   
 <#assign classNameLower = className?uncap_first> 
-package ${basepackage}.service.mapper.auto.dataobject;
+package ${basepackage}.service.mapper.dataobject;
 
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import lombok.Data;
-import org.apache.ibatis.features.jpa.annotation.Entity;
-import org.apache.ibatis.features.jpa.annotation.Table;
-import org.apache.ibatis.features.jpa.annotation.Id;
-import org.apache.ibatis.features.jpa.annotation.Column;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mybatis.annotations.Column;
+import org.springframework.data.mybatis.annotations.Entity;
 
 
 /**
@@ -22,19 +22,14 @@ import org.apache.ibatis.features.jpa.annotation.Column;
  *
  */
 @Data
-@Entity
-@Table(name="${table.sqlName}")
+@Entity(table="${table.sqlName}")
 public class ${className}DO {
 
     <#list table.columns as column>
     /**
      * ${column.remarks}
      */
-    <#if column.pk>
-    @Id
-    <#else>
-    @Column<#if !column.updatable || column.columnName == "CreateTime" && column.columnName == "LastUpdateTime" && column.columnName == "Id">(updatable=false, insertable=false)</#if>
-    </#if>
+    <#if column.pk>@Id<#else><#if !column.updatable || !column.insertable || column.columnName == "CreateTime" && column.columnName == "LastUpdateTime" && column.columnName == "Id">@Column(updatable=false, insertable=false)</#if></#if>
     private ${column.simpleJavaType} ${column.columnNameLower};
     </#list>
 
