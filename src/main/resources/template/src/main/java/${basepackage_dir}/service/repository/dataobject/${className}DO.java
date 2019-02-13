2eb60ef97c84b74aad851186de9bfb83157141cc
王龙 <wanglong@migu.cn>
2019-02-13 11:14:09 +0800

    修正DO继承关系

--- src/main/resources/template/src/main/java/${basepackage_dir}/service/repository/dataobject/${className}DO.java
+++ src/main/resources/template/src/main/java/${basepackage_dir}/service/repository/dataobject/${className}DO.java
@@ -1,6 +1,6 @@
 <#include "/java_copyright.include">
-<#assign className = table.className>   
-<#assign classNameLower = className?uncap_first> 
+<#assign className = table.className>
+<#assign classNameLower = className?uncap_first>
 package ${basepackage}.service.repository.dataobject;
 
 import java.util.Date;
@@ -11,26 +11,27 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mybatis.annotations.Column;
 import org.springframework.data.mybatis.annotations.Entity;
-
+import cn.migu.music.common.dataobject.BaseDO;
 
 /**
- * 
+ *
  * ${table.remarks} 数据库表映射
- * 
+ *
  * @author generator
  * @version ${now?string('yyyy-MM-dd HH:mm:ss')}
  *
  */
 @Data
 @Entity(table="${table.sqlName}")
-public class ${className}DO {
+public class ${className}DO extends BaseDO<${table.idColumn.simpleJavaType}> {
 
     <#list table.columns as column>
+    <#if column.columnName != "CreatedTime" && column.columnName != "LastModifiedTime" && column.columnName != "Id">
     /**
      * ${column.remarks}
      */
-    <#if column.pk>@Id<#else><#if !column.updatable || !column.insertable || column.columnName == "CreateTime" && column.columnName == "LastUpdateTime" && column.columnName == "Id">@Column(updatable=false, insertable=false)</#if></#if>
     private ${column.simpleJavaType} ${column.columnNameLower};
+    </#if>
     </#list>
 
 }
